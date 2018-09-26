911ef7bb790b5e568f01f1446297a4c13aea25e0
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-26 20:35:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |count|
-    if count == "X" || "O"
+    if count == "X"
+      counter += 1
+    elsif count == "O"
       counter += 1
     end
   end
